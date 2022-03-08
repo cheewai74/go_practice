a82eb174e024d966ab827d2779641d73e99955d6
moses <academicsnotes@gmail.com>
2022-03-08 11:57:29 +0800

    Commit script

--- Basic/numbers_and_assignment_example1.go
+++ Basic/numbers_and_assignment_example1.go
@@ -6,16 +6,22 @@ import (
 )
 
 func main() {
-	var x int
-	var y int
+	// var x int
+	// var x float64
+	// var y int
+	// var y float64
 
-	x = 1
-	y = 2
+	// x = 1
+	// y = 2
+
+	x, y := 1.0, 2.0
 
 	fmt.Printf("x=%v, type of %T\n", x, x)
 	fmt.Printf("y=%v, type of %T\n", y, y)
 
-	var mean int
-	mean = (x + y) / 2
+	// var mean int
+
+	// mean = (x + y) / 2
+	mean := (x + y) / 2.0
 	fmt.Printf("result: %v, type of %T\n", mean, mean)
 }
